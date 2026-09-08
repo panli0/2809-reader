07d82e0eef748a7c9a2298fa9d7fedb72a0599e7
Pan Li <152784731+panli0@users.noreply.github.com>
2026-09-09 01:13:29 +0800

    Update offline cache for 9 chapter files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='river-year-v3';
-const ASSETS=["./","./icon.svg","./index.html","./manifest.json","app.js","chapters-1.js","chapters-2.js","chapters-3.js","chapters-4.js","collocations-1.js","data-init.js","headwords.js","ipa-1.js","ipa-2.js","ipa-3.js","ipa-4.js","poly-1.js","poly-2.js","surface-1.js","surface-2.js","surface-3.js","sw.js"];
+const CACHE='river-year-v4';
+const ASSETS=['./','./icon.svg','./index.html','./manifest.json','./app.js','./data-init.js','./chapter-1.js','./chapter-2.js','./chapter-3.js','./chapter-4.js','./chapter-5.js','./chapter-6.js','./chapter-7.js','./chapter-8.js','./chapter-9.js','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}))) });
